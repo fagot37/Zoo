7e273549f3f51e56babe89629d15a1f0a663c8e2
def <fagot37@mail.ru>
2018-11-01 01:21:13 +0300

    Hippo

--- Zoo.java
+++ Zoo.java
@@ -2,5 +2,6 @@ public class Zoo{
     public static void main(String ... args){
         System.out.println("Welcom to ZOO");
         Zebra zebra = new Zebra();
+        Hippo hippo = new Hippo();
     }
 }
